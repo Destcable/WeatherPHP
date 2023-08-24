0a98ffbdc8a146dc87b0104aea4f99fe85d97a0e
Destcable <artmobpavlov21@gmail.com>
2023-08-24 15:18:55 +0300

    [

--- src/Weather.php
+++ src/Weather.php
@@ -5,18 +5,24 @@ namespace WeatherPHP;
 class Weather
 {
     private string $apiKey;
+    private array $data;
     
     public function __construct(string $apiKey)
     {
         $this->apiKey = $apiKey;
     }
 
-    public function find(string $search)
+    public function find(string $search): array
     {
         $client = new Client($this->apiKey);
 
-        return $client->get(
-            $search,
-        );
+        $this->data = $client->get($search);
+
+        return $this->data;
+    }
+
+    public function getTemperature(): float
+    {
+        return $this->data['main']['temp'];
     }
 }
