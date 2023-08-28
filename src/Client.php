ce7eb17adda41dc0a0647a0a786123c3f864666d
Destcable <artmobpavlov21@gmail.com>
2023-08-28 15:12:10 +0300

    [UPD] get day's temperature

--- src/Client.php
+++ src/Client.php
@@ -4,18 +4,8 @@ namespace WeatherPHP;
 
 final class Client
 {
-    private string $apiKey;
-    private string $language;
-
-    public function __construct(string $apiKey, string $language)
+    public static function get(string $url)
     {
-        $this->apiKey = $apiKey;
-        $this->language = $language;
-    }
-
-    public function get(string $city)
-    {
-        $url = "https://api.openweathermap.org/data/2.5/weather?q={$city}&appid={$this->apiKey}&lang={$this->language}&units=metric";
         $response = file_get_contents($url);
 
         return json_decode($response, true);
--- src/Weather.php
+++ src/Weather.php
@@ -5,6 +5,7 @@ namespace WeatherPHP;
 class Weather
 {
     public string $language = 'en';
+
     private string $apiKey;
     private array $data;
     
@@ -13,13 +14,32 @@ class Weather
         $this->apiKey = $apiKey;
     }
 
-    public function find(string $search): array
+    public function find(string $city): array
     {
-        $client = new Client($this->apiKey, $this->language);
-
-        $this->data = $client->get($search);
+        $this->data = Client::get("https://api.openweathermap.org/data/2.5/weather?q={$city}&appid={$this->apiKey}&lang={$this->language}&units=metric");
 
         return $this->data;
+    }
+
+    public function get5dTemperature(string $city): array
+    {
+        $this->data = Client::get("http://api.openweathermap.org/data/2.5/forecast?q={$city}&appid={$this->apiKey}&lang={$this->language}&units=metric");
+        
+        $dataTemp = [];
+
+        for ($i=0; $i < count($this->data['list']); $i++) { 
+            
+            $dataTemp []= [
+                'date' => $this->data['list'][$i]['dt_txt'],
+                'temperature' => $this->data['list'][$i]['main']['temp'],
+                'wind_speed' => $this->data['list'][$i]['wind']['speed'],
+                'weather_main' => $this->data['list'][$i]['weather'][0]['main'],
+                'weather_description' => $this->data['list'][$i]['weather'][0]['description'],
+            ];
+        
+        }
+
+        return $dataTemp;
     }
 
     public function getTemperature(): float
